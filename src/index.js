4ec816b07617bb260073e010406136c3a2b30a33
Sergei Odintsov <loveravel@yandex.ru>
2020-03-06 12:02:24 +0300

    fix: changed import mocks

--- src/index.js
+++ src/index.js
@@ -1,11 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import films from "./mocks/films.js";
+import {films, comments} from "./mocks/films.js";
 import App from "./components/app/app.jsx";
 
 ReactDOM.render(
     <App
       films={films}
+      comments={comments}
     />,
     document.getElementById(`root`)
 );
