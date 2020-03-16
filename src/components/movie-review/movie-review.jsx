28a0b7d3886382b35272cfbde23424473f57ddcb
Sergei Odintsov <loveravel@yandex.ru>
2020-03-16 09:34:19 +0300

    feat: added component movie-review

---
+++ src/components/movie-review/movie-review.jsx
@@ -0,0 +1,33 @@
+import React from "react";
+import PropTypes from "prop-types";
+
+const MovieReview = (props) => {
+  const {user, rating, comment, date} = props;
+
+  return (
+    <div className="review">
+      <blockquote className="review__quote">
+        <p className="review__text">{comment}</p>
+
+        <footer className="review__details">
+          <cite className="review__author">{user.name}</cite>
+          <time className="review__date" dateTime="2016-12-24">{date}</time>
+        </footer>
+      </blockquote>
+
+      <div className="review__rating">{rating}</div>
+    </div>
+  );
+};
+
+MovieReview.propTypes = {
+  user: PropTypes.shape({
+    id: PropTypes.number,
+    name: PropTypes.string,
+  }),
+  rating: PropTypes.number,
+  comment: PropTypes.string,
+  date: PropTypes.string,
+};
+
+export default MovieReview;
--- src/components/movie-reviews/movie-reviews.jsx
+++ src/components/movie-reviews/movie-reviews.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import PropTypes from "prop-types";
+import MovieReview from "../movie-review/movie-review.jsx";
 
 const MovieReviews = (props) => {
   const {reviews} = props;
@@ -8,93 +9,32 @@ const MovieReviews = (props) => {
     <div className="movie-card__reviews movie-card__row">
       <div className="movie-card__reviews-col">
         {reviews.map((review, index) =>
-          <div className="review" key={`${review.name}-${index}`}>
-            <blockquote className="review__quote">
-              <p className="review__text">{review.text}</p>
-
-              <footer className="review__details">
-                <cite className="review__author">{review.name}</cite>
-                <time className="review__date" dateTime="2016-12-24">December 24, 2016</time>
-              </footer>
-            </blockquote>
-
-            <div className="review__rating">{review.rating}</div>
-          </div>
+          <MovieReview
+            key={`${review.date}-${index}`}
+            user={review.user}
+            rating={review.rating}
+            comment={review.comment}
+            date={review.date}
+          />
         )}
-
-
-        <div className="review">
-          <blockquote className="review__quote">
-            <p className="review__text">Anderson&apos;s films are too precious for some, but for those of us willing to lose ourselves in them, they&apos;re a delight. &quot;The Grand Budapest Hotel&quot; is no different, except that he has added a hint of gravitas to the mix, improving the recipe.</p>
-
-            <footer className="review__details">
-              <cite className="review__author">Bill Goodykoontz</cite>
-              <time className="review__date" dateTime="2015-11-18">November 18, 2015</time>
-            </footer>
-          </blockquote>
-
-          <div className="review__rating">8,0</div>
-        </div>
-
-        <div className="review">
-          <blockquote className="review__quote">
-            <p className="review__text">I didn&apos;t find it amusing, and while I can appreciate the creativity, it&apos;s an hour and 40 minutes I wish I could take back.</p>
-
-            <footer className="review__details">
-              <cite className="review__author">Amanda Greever</cite>
-              <time className="review__date" dateTime="2015-11-18">November 18, 2015</time>
-            </footer>
-          </blockquote>
-
-          <div className="review__rating">8,0</div>
-        </div>
       </div>
       <div className="movie-card__reviews-col">
-        <div className="review">
-          <blockquote className="review__quote">
-            <p className="review__text">The mannered, madcap proceedings are often delightful, occasionally silly, and here and there, gruesome and/or heartbreaking.</p>
-
-            <footer className="review__details">
-              <cite className="review__author">Matthew Lickona</cite>
-              <time className="review__date" dateTime="2016-12-20">December 20, 2016</time>
-            </footer>
-          </blockquote>
-
-          <div className="review__rating">7,2</div>
-        </div>
-
-        <div className="review">
-          <blockquote className="review__quote">
-            <p className="review__text">It is certainly a magical and childlike way of storytelling, even if the content is a little more adult.</p>
-
-            <footer className="review__details">
-              <cite className="review__author">Paula Fleri-Soler</cite>
-              <time className="review__date" dateTime="2016-12-20">December 20, 2016</time>
-            </footer>
-          </blockquote>
-
-          <div className="review__rating">7,6</div>
-        </div>
-
-        <div className="review">
-          <blockquote className="review__quote">
-            <p className="review__text">It is certainly a magical and childlike way of storytelling, even if the content is a little more adult.</p>
-
-            <footer className="review__details">
-              <cite className="review__author">Paula Fleri-Soler</cite>
-              <time className="review__date" dateTime="2016-12-20">December 20, 2016</time>
-            </footer>
-          </blockquote>
-
-          <div className="review__rating">7,0</div>
-        </div>
+        {reviews.map((review, index) =>
+          <MovieReview
+            key={`${review.date}-${index}`}
+            user={review.user}
+            rating={review.rating}
+            comment={review.comment}
+            date={review.date}
+          />
+        )}
       </div>
     </div>
   );
 };
 
 MovieReviews.propTypes = {
-  reviews: PropTypes.array
+  reviews: PropTypes.array,
 };
 
 export default MovieReviews;
