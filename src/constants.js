539b878d11c8ebdd79d6129d1259bcbcc66574fd
Sergei Odintsov <loveravel@yandex.ru>
2020-03-29 18:57:50 +0300

    feat: added file for constants

---
+++ src/constants.js
@@ -0,0 +1,19 @@
+export const Tab = {
+  OVERVIEW: `Overview`,
+  DETAILS: `Details`,
+  REVIEWS: `Reviews`,
+};
+
+export const FilmsCount = {
+  STEP: 8,
+  INITIAL_MAIN: 8,
+  INITIAL_SIMILAR: 4,
+};
+
+export const RatingDescription = {
+  BAD: `Bad`,
+  NORMAL: `Normal`,
+  GOOD: `Good`,
+  VERY_GOOD: `Very Good`,
+  AWESOME: `Awesome`,
+};
