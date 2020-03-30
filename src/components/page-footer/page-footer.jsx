e23376a7c8e3af870bdedec73c9fefc8447c6f55
Sergei Odintsov <loveravel@yandex.ru>
2020-03-30 17:37:45 +0300

    feat: added logotype component

--- src/components/page-footer/page-footer.jsx
+++ src/components/page-footer/page-footer.jsx
@@ -1,16 +1,11 @@
 import React from "react";
+import Logotype from "../logotype/logotype";
 
 const PageFooter = () => {
 
   return (
     <footer className="page-footer">
-      <div className="logo">
-        <a href="main.html" className="logo__link logo__link--light">
-          <span className="logo__letter logo__letter--1">W</span>
-          <span className="logo__letter logo__letter--2">T</span>
-          <span className="logo__letter logo__letter--3">W</span>
-        </a>
-      </div>
+      <Logotype/>
 
       <div className="copyright">
         <p>© 2019 What to watch Ltd.</p>
--- src/components/page-header/page-header.jsx
+++ src/components/page-header/page-header.jsx
@@ -1,18 +1,14 @@
 import React from "react";
 import PropTypes from "prop-types";
+import Logotype from "../logotype/logotype.jsx";
 
 const PageHeader = (props) => {
   const {avatar} = props;
 
   return (
     <header className="page-header movie-card__head">
-      <div className="logo">
-        <a className="logo__link">
-          <span className="logo__letter logo__letter--1">W</span>
-          <span className="logo__letter logo__letter--2">T</span>
-          <span className="logo__letter logo__letter--3">W</span>
-        </a>
-      </div>
+
+      <Logotype/>
 
       <div className="user-block">
         <div className="user-block__avatar">
