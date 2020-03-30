25d727280374d8f38beecc831d6babbbe06c3318
Sergei Odintsov <loveravel@yandex.ru>
2020-03-30 12:46:29 +0300

    feat: added component movie-promo

---
+++ src/components/movie-promo/movie-promo.jsx
@@ -0,0 +1,77 @@
+import React from "react";
+import PropTypes from "prop-types";
+
+import PageHeader from "../page-header/page-header.jsx";
+
+const MoviePromo = (props) => {
+  const {name, posterImage, backgroundImage, genre, released, avatar} = props;
+  return (
+    <section className="movie-card">
+      <div className="movie-card__bg">
+        <img
+          src={backgroundImage}
+          alt={name}
+        />
+      </div>
+
+      <h1 className="visually-hidden">WTW</h1>
+
+      <PageHeader
+        avatar={avatar}
+      />
+
+      <div className="movie-card__wrap">
+        <div className="movie-card__info">
+          <div className="movie-card__poster">
+            <img
+              src={posterImage}
+              alt={`${name} poster`}
+              width="218"
+              height="327"
+            />
+          </div>
+
+          <div className="movie-card__desc">
+            <h2 className="movie-card__title">{name}</h2>
+            <p className="movie-card__meta">
+              <span className="movie-card__genre">{genre}</span>
+              <span className="movie-card__year">{released}</span>
+            </p>
+
+            <div className="movie-card__buttons">
+              <button
+                className="btn btn--play movie-card__button"
+                type="button"
+              >
+                <svg viewBox="0 0 19 19" width="19" height="19">
+                  <use xlinkHref="#play-s"></use>
+                </svg>
+                <span>Play</span>
+              </button>
+              <button
+                className="btn btn--list movie-card__button"
+                type="button"
+              >
+                <svg viewBox="0 0 19 20" width="19" height="20">
+                  <use xlinkHref="#add"></use>
+                </svg>
+                <span>My list</span>
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+MoviePromo.propTypes = {
+  name: PropTypes.string,
+  posterImage: PropTypes.string,
+  backgroundImage: PropTypes.string,
+  genre: PropTypes.string,
+  released: PropTypes.number,
+  avatar: PropTypes.string
+};
+
+export default MoviePromo;
