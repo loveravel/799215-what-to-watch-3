c5972d9a3db505798be03cb83748068cb79f4376
Sergei Odintsov <loveravel@yandex.ru>
2020-04-05 17:06:22 +0300

    feat: added snaphot test for GenreList

---
+++ src/components/genre-list/genre-list.test.js
@@ -0,0 +1,16 @@
+import React from "react";
+import renderer from "react-test-renderer";
+import GenreList from "../genre-list/genre-list.jsx";
+
+it(`<GenresList/> is rendered correctly`, () => {
+  const props = {
+    genres: [`All genres`, `Crime`, `Drama`, `Action`, `Fantasy`, `Adventure`],
+    genreFilter: `Crime`,
+    onGenreClick: () => {}
+  };
+
+  const tree = renderer
+    .create(<GenreList {...props}/>)
+    .toJSON();
+  expect(tree).toMatchSnapshot();
+});
