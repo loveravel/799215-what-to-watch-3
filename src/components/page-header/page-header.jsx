9f5489f6374f4b857a2b06842b7ff7f392008b89
Sergei Odintsov <loveravel@yandex.ru>
2020-04-05 12:19:46 +0300

    feat: added the ability to accept an additional class

--- src/components/page-header/page-header.jsx
+++ src/components/page-header/page-header.jsx
@@ -1,15 +1,21 @@
 import React from "react";
+import PropTypes from "prop-types";
 
 import Logotype from "../logotype/logotype.jsx";
 import UserBlock from "../user-block/user-block.jsx";
 
-const PageHeader = () => {
+const PageHeader = (props) => {
+  const {additionalClass} = props;
   return (
-    <header className="page-header movie-card__head">
+    <header className={`page-header ${additionalClass}`}>
       <Logotype/>
       <UserBlock/>
     </header>
   );
+};
+
+PageHeader.propTypes = {
+  additionalClass: PropTypes.string.isRequired,
 };
 
 export default PageHeader;
