beafd93b2c3044dfafd95619fccd3dc73e8ac077
Sergei Odintsov <loveravel@yandex.ru>
2020-04-05 17:13:42 +0300

    feat: corrected snaphot test for PageFooter

--- src/components/page-footer/page-footer.test.js
+++ src/components/page-footer/page-footer.test.js
@@ -1,11 +1,18 @@
 import React from "react";
 import renderer from "react-test-renderer";
+import {Router} from 'react-router-dom';
+
+import history from "../../history.js";
+
 import PageFooter from "./page-footer.jsx";
 
 it(`<PageFooter /> is rendered correctly`, () => {
   const tree = renderer
-    .create(<PageFooter />)
-    .toJSON();
+    .create(
+        <Router history={history}>
+          <PageFooter/>
+        </Router>
+    ).toJSON();
 
   expect(tree).toMatchSnapshot();
 });
