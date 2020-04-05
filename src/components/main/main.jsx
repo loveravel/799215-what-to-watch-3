e60c2332f7886572ccb8e17cb71b209005fb4cd0
Sergei Odintsov <loveravel@yandex.ru>
2020-04-05 12:17:31 +0300

    feat: changed render movie cards

--- src/components/main/main.jsx
+++ src/components/main/main.jsx
@@ -1,12 +1,21 @@
 import React from "react";
 import PropTypes from "prop-types";
+import {connect} from "react-redux";
+
+import {ActionCreator} from "../../reducer/data/data.js";
+import {getFilmsCount, getCurrentGenre, getPromoMovie, getFilteredFilms, getGenres} from "../../reducer/data/selectors.js";
 
 import PageFooter from "../page-footer/page-footer.jsx";
-import Catalog from "../catalog/catalog.jsx";
 import MoviePromo from "../movie-promo/movie-promo.jsx";
+import PageHeader from "../page-header/page-header.jsx";
+import ButtonMore from "../button-more/button-more.jsx";
+import GenreList from "../genre-list/genre-list.jsx";
+import MovieList from "../movie-list/movie-list.jsx";
+
 
 const Main = (props) => {
-  const {movie} = props;
+  const {films, filmsCount, movie, currentGenre, genres} = props;
+  const {onGenreClick} = props;
 
   if (!movie) {
     return null;
@@ -14,21 +23,49 @@ const Main = (props) => {
 
   return (
     <React.Fragment>
-      <MoviePromo
-        name={movie.name}
-        posterImage={movie.posterImage}
-        backgroundImage={movie.backgroundImage}
-        genre={movie.genre}
-        released={movie.released}
-        avatar={`img/avatar.jpg`}
-      />
+      <section className="movie-card">
+        <div className="movie-card__bg">
+          <img
+            src={movie.backgroundImage}
+            alt={movie.name}
+          />
+        </div>
+
+        <h1 className="visually-hidden">WTW</h1>
+
+        <PageHeader additionalClass="movie-card__head" />
+
+        <MoviePromo
+          id={movie.id}
+          name={movie.name}
+          posterImage={movie.posterImage}
+          genre={movie.genre}
+          released={movie.released}
+          isFavorite={movie.isFavorite}
+        />
+      </section>
 
       <div className="page-content">
+        <section className="catalog">
+          <h2 className="catalog__title visually-hidden">Catalog</h2>
 
-        <Catalog />
+          <GenreList
+            genres={genres}
+            genreFilter={currentGenre}
+            onGenreClick={onGenreClick}
+          />
 
-        <PageFooter />
+          <MovieList
+            films={films}
+            genreFilter={currentGenre}
+            filmsCount={filmsCount}
+            message="No films"
+          />
 
+          <ButtonMore/>
+        </section>
+
+        <PageFooter/>
       </div>
     </React.Fragment>
   );
@@ -36,12 +73,39 @@ const Main = (props) => {
 
 Main.propTypes = {
   movie: PropTypes.shape({
+    id: PropTypes.number,
     name: PropTypes.string,
     posterImage: PropTypes.string,
     backgroundImage: PropTypes.string,
     genre: PropTypes.string,
     released: PropTypes.number,
+    isFavorite: PropTypes.bool,
   }),
+  films: PropTypes.arrayOf(PropTypes.shape({
+    name: PropTypes.string,
+    posterImage: PropTypes.string,
+    genre: PropTypes.string,
+  })),
+  filmsCount: PropTypes.number,
+  currentGenre: PropTypes.string,
+  onGenreClick: PropTypes.func,
+  genres: PropTypes.arrayOf(PropTypes.string),
 };
 
-export default Main;
+const mapStateToProps = (state) => ({
+  films: getFilteredFilms(state),
+  promoFilm: getPromoMovie(state),
+  currentGenre: getCurrentGenre(state),
+  filmsCount: getFilmsCount(state),
+  genres: getGenres(state),
+});
+
+const mapDispatchToProps = (dispatch) => ({
+  onGenreClick(genre) {
+    dispatch(ActionCreator.changeGenre(genre));
+  }
+});
+
+export {Main};
+
+export default connect(mapStateToProps, mapDispatchToProps)(Main);
