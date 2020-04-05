1b96d01c2e4336b01b40fc1e17704251d1374533
Sergei Odintsov <loveravel@yandex.ru>
2020-04-05 18:11:03 +0300

    feat: finished MovieDetails & added snapshot test

--- src/components/movie-details/movie-details.jsx
+++ src/components/movie-details/movie-details.jsx
@@ -1,6 +1,8 @@
 import React from "react";
 import PropTypes from "prop-types";
 
+import {makeDuration} from "../../utils.js";
+
 const MovieDetails = (props) => {
   const {movie} = props;
 
@@ -8,24 +10,13 @@ const MovieDetails = (props) => {
     <div className="movie-card__text movie-card__row">
       <div className="movie-card__text-col">
         <p className="movie-card__details-item">
-          <strong className="movie-card__details-name">{movie.director}</strong>
-          <span className="movie-card__details-value">Wes Andreson</span>
+          <strong className="movie-card__details-name">Director</strong>
+          <span className="movie-card__details-value">{movie.director}</span>
         </p>
         <p className="movie-card__details-item">
           <strong className="movie-card__details-name">Starring</strong>
           <span className="movie-card__details-value">
-            Bill Murray, <br/>
-            Edward Norton, <br/>
-            Jude Law, <br/>
-            Willem Dafoe, <br/>
-            Saoirse Ronan, <br/>
-            Tony Revoloru, <br/>
-            Tilda Swinton, <br/>
-            Tom Wilkinson, <br/>
-            Owen Wilkinson, <br/>
-            Adrien Brody, <br/>
-            Ralph Fiennes, <br/>
-            Jeff Goldblum
+            {movie.starring.map((star) => `${star}\n`)}
           </span>
         </p>
       </div>
@@ -33,15 +24,15 @@ const MovieDetails = (props) => {
       <div className="movie-card__text-col">
         <p className="movie-card__details-item">
           <strong className="movie-card__details-name">Run Time</strong>
-          <span className="movie-card__details-value">1h 39m</span>
+          <span className="movie-card__details-value">{makeDuration(movie.runTime)}</span>
         </p>
         <p className="movie-card__details-item">
           <strong className="movie-card__details-name">Genre</strong>
-          <span className="movie-card__details-value">Comedy</span>
+          <span className="movie-card__details-value">{movie.genre}</span>
         </p>
         <p className="movie-card__details-item">
           <strong className="movie-card__details-name">Released</strong>
-          <span className="movie-card__details-value">2014</span>
+          <span className="movie-card__details-value">{movie.released}</span>
         </p>
       </div>
     </div>
--- src/components/movie-details/movie-details.test.js
+++ src/components/movie-details/movie-details.test.js
@@ -1,45 +1,17 @@
 import React from "react";
 import renderer from "react-test-renderer";
+
+import {movie} from "../../mocks/test-data.js";
+
 import MovieDetails from "./movie-details.jsx";
 
-const movie = {
-  id: 1,
-  name: `Aviator`,
-  posterImage: `img/the-grand-budapest-hotel-poster.jpg`,
-  previewImage: `img/aviator.jpg`,
-  backgroundImage: `img/bg-the-grand-budapest-hotel.jpg`,
-  previewVideo: `https://`,
-  video: `https://`,
-  genre: `Drama / Epic Movie`,
-  released: 2014,
-  description: `Having received a small factory from his father,
-    Howard Hughes turned it into a gigantic, fantastically profitable enterprise.
-    Having become the owner of a huge film company, he shot the most expensive
-    film of his time and won the hearts of the most beautiful Hollywood actresses.`,
-  director: `Martin Scorsese`,
-  starring: [
-    `Leonardo DiCaprio`,
-    `Cate Blanchett`,
-    `Kate Beckinsale`,
-    `John C. Reilly`,
-    `Alec Baldwin`,
-    `Alan Alda`,
-    `Ian Holm`,
-    `Danny Huston`,
-    `Gwen Stefani`,
-    `Jude Law`,
-  ],
-  runTime: 170,
-  rating: 9.5,
-  scoresCount: 15,
-  isFavorite: false,
-};
-
 it(`<MovieDetails /> is rendered correctly`, () => {
+  const props = {
+    movie,
+  };
+
   const tree = renderer
-    .create(<MovieDetails
-      movie={movie}
-    />)
+    .create(<MovieDetails {...props}/>)
     .toJSON();
 
   expect(tree).toMatchSnapshot();
