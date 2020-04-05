6ea944ae12fcc0bde219ad2ef1743149de4a4688
Sergei Odintsov <loveravel@yandex.ru>
2020-04-05 22:06:00 +0300

    feat: added routing &  snapshot test for App

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -1,18 +1,30 @@
 import React from "react";
 import PropTypes from "prop-types";
 import {Router, Route, Switch} from "react-router-dom";
-import history from "../../history.js";
 import {connect} from "react-redux";
 
 import {getPromoMovie} from "../../reducer/data/selectors.js";
 
 import withAuthForm from "../../hocs/with-auth-form/with-auth-form.js";
+import withVideoPlayer from "../../hocs/with-video-player/with-video-player.js";
+import withFormReview from "../../hocs/with-form-review/with-form-review.js";
 
+import {AppRoute} from "../../constants.js";
+import history from "../../history.js";
+
+import PrivateRoute from "../private-route/private-route.jsx";
 import Main from "../main/main.jsx";
 import MoviePage from "../movie-page/movie-page.jsx";
 import SignIn from "../sign-in/sign-in.jsx";
+import MyList from "../my-list/my-list.jsx";
+import VideoPlayer from "../video-player/video-player.jsx";
+import AddReview from "../add-review/add-review.jsx";
+
 
 const SignInWrapped = withAuthForm(SignIn);
+const WrappedVideoPlayer = withVideoPlayer(VideoPlayer);
+const WrappedAddReview = withFormReview(AddReview);
+
 
 const App = (props) => {
   const {promoMovie} = props;
@@ -20,12 +32,57 @@ const App = (props) => {
   return (
     <Router history={history}>
       <Switch>
-        <Route exact path="/">
+        <Route exact path={AppRoute.ROOT}>
           <Main movie={promoMovie}/>
         </Route>
-        <Route exact path="/login">
-          <SignInWrapped />
+        <Route exact path={AppRoute.LOGIN}>
+          <SignInWrapped/>
         </Route>
+        <PrivateRoute
+          exact
+          path={AppRoute.MY_LIST}
+          render={() => {
+            return (
+              <MyList/>
+            );
+          }}
+        />
+        <Route
+          exact
+          path={AppRoute.MOVIE}
+          render={(prop) => {
+            return (
+              <MoviePage
+                {...prop}
+              />
+            );
+          }}
+        />
+        <Route
+          path={AppRoute.VIDEO_PLAYER}
+          exact
+          render={(prop) => {
+            return (
+              <WrappedVideoPlayer
+                isPlaying={true}
+                isMuted={false}
+                movieID={prop.match.params.id}
+                isPreviewMode={false}
+              />
+            );
+          }}
+        />
+        <PrivateRoute
+          exact
+          path={AppRoute.ADD_REVIEW}
+          render={(prop) => {
+            return (
+              <WrappedAddReview
+                movieID={prop.match.params.id}
+              />
+            );
+          }}
+        />
       </Switch>
     </Router>
   );
