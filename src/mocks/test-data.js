897d66a7c240b30e3c7f22c9aa0ae10143db8fa4
Sergei Odintsov <loveravel@yandex.ru>
2020-04-05 20:05:03 +0300

    feat:  added test data

---
+++ src/mocks/test-data.js
@@ -0,0 +1,119 @@
+export const films = [
+  {
+    id: 1,
+    name: `Movie 1`,
+    genre: ``,
+    released: 2019,
+    backgroundImage: ``,
+    backgroundColor: ``,
+    previewImage: ``,
+    posterImage: ``,
+    description: ``,
+    rating: 4.5,
+    scoresCount: 300,
+    runTime: 3,
+    director: ``,
+    starring: [],
+    previewVideo: ``,
+    video: ``,
+    isFavorite: true
+  },
+  {
+    id: 2,
+    name: `Movie 2`,
+    genre: ``,
+    released: 2019,
+    backgroundImage: ``,
+    backgroundColor: ``,
+    previewImage: ``,
+    posterImage: ``,
+    description: ``,
+    rating: 4.5,
+    scoresCount: 300,
+    runTime: 3,
+    director: ``,
+    starring: [],
+    previewVideoLink: ``,
+    video: ``,
+    isFavorite: false
+  },
+  {
+    id: 3,
+    name: `Movie 3`,
+    genre: `Drama`,
+    released: 2019,
+    backgroundImage: ``,
+    backgroundColor: ``,
+    previewImage: ``,
+    posterImage: ``,
+    description: ``,
+    rating: 4.5,
+    scoresCount: 300,
+    runTime: 3,
+    director: ``,
+    starring: [],
+    previewVideo: ``,
+    video: ``,
+    isFavorite: false
+  },
+];
+
+export const movie = {
+  id: 1,
+  name: `Movie 1`,
+  genre: ``,
+  released: 2019,
+  backgroundImage: ``,
+  backgroundColor: ``,
+  previewImage: ``,
+  posterImage: ``,
+  description: ``,
+  rating: 4.5,
+  scoresCount: 300,
+  runTime: 3,
+  director: ``,
+  starring: [],
+  previewVideo: ``,
+  video: ``,
+  isFavorite: false
+};
+
+export const reviews = [
+  {
+    id: 1,
+    user: {
+      id: 1,
+      name: ``
+    },
+    rating: 5.5,
+    comment: ``,
+    date: `2019-05-08T14:13:56.569Z`
+  },
+  {
+    id: 2,
+    user: {
+      id: 1,
+      name: ``
+    },
+    rating: 5.5,
+    comment: ``,
+    date: `2019-05-08T14:13:56.569Z`
+  },
+  {
+    id: 3,
+    user: {
+      id: 1,
+      name: ``
+    },
+    rating: 5.5,
+    comment: ``,
+    date: `2019-05-08T14:13:56.569Z`
+  }
+];
+
+export const userData = {
+  id: 1,
+  name: ``,
+  email: ``,
+  avatarUrl: ``
+};
